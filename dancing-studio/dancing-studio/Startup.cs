01bc48d9ab880f3bed6eb55524e8f910a51620bf
Aleksandra <sasha.bogma@gmail.com>
2015-10-16 23:47:22 +0300

    Init

---
+++ dancing-studio/dancing-studio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dancing_studio.Startup))]
+namespace dancing_studio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
